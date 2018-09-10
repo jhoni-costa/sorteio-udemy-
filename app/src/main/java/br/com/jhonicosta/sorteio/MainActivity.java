50f367493c22fc4819dfcfb9c04d33f0fb6e7f8e
jhoni-costa <jhoni_costa@outlook.com>
2018-09-10 18:05:53 -0300

    Finalizando App

--- app/src/main/java/br/com/jhonicosta/sorteio/MainActivity.java
+++ app/src/main/java/br/com/jhonicosta/sorteio/MainActivity.java
@@ -2,12 +2,35 @@ package br.com.jhonicosta.sorteio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button jogar;
+    private TextView resposta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jogar = findViewById(R.id.button_jogar);
+        resposta = findViewById(R.id.resposta);
+
+        jogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                int numero = random.nextInt(11);
+
+                resposta.setText("Número escolhido: " + numero);
+            }
+        });
+
     }
 }
